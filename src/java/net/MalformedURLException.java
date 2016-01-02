7d9d1f76dbe141e5c78c65b461dc20c78c15421a
Colby Skeggs <git@colbyskeggs.com>
2016-01-01 18:08:29 -0800

    Add bare-bones URI and URI

---
+++ src/java/net/MalformedURLException.java
@@ -0,0 +1,14 @@
+package java.net;
+
+import java.io.IOException;
+
+public class MalformedURLException extends IOException {
+
+	public MalformedURLException() {
+		super();
+	}
+
+	public MalformedURLException(String message) {
+		super(message);
+	}
+}
---
+++ src/java/net/URI.java
@@ -0,0 +1,9 @@
+package java.net;
+
+public class URI implements Comparable<URI> /* , Serializable */ {
+	// TODO
+
+	public URL toURL() throws MalformedURLException {
+		throw new IncompleteImplementationError();
+	}
+}
